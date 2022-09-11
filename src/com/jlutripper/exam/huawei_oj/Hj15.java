46a1c906466b7f6720c9dc164adf08a53c9494bc
jlutripper <jlutripper@gmail.com>
2022-09-11 18:31:27 +0800

    9.11 surface

--- src/com/jlutripper/exam/huawei_oj/Hj15.java
+++ src/com/jlutripper/exam/huawei_oj/Hj15.java
@@ -1,5 +1,7 @@
 package com.jlutripper.exam.huawei_oj;
 
+import java.util.Scanner;
+
 /**
  * @description: HJ15 求int型正整数在内存中存储时1的个数
  * @author: Jlutripper
@@ -8,5 +10,12 @@ package com.jlutripper.exam.huawei_oj;
 
 
 public class Hj15 {
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = Integer.toBinaryString(n);
+        int ans = 0;
+        for (Character c : s.toCharArray()) if (c == '1') ans++;
+        System.out.println(ans);
+    }
 }
